132382c8f72522325d877a7ade0d0e1faba1cb2d
faruk <studentfaruk0308@gmail.com>
2022-03-29 21:02:09 +1100

    After Completion

---
+++ index.js
@@ -0,0 +1,7 @@
+let container = document.querySelector('#container');
+
+let clickable = document.querySelector('#clickable');
+
+clickable.addEventListener('click', () => {
+    window.alert("Hello")
+});
