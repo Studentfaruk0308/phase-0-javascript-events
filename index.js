3a2725e6c62140cf6e8b224e41d34701a7e00e7a
faruk <studentfaruk0308@gmail.com>
2022-03-30 14:29:04 +1100

    After Making Red

--- index.js
+++
@@ -1,7 +0,0 @@
-let container = document.querySelector('#container');
-
-let clickable = document.querySelector('#clickable');
-
-clickable.addEventListener('click', () => {
-    window.alert("Hello")
-});
